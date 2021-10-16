b17fe6c132471828dff2fdef5cbd53aaf2f85075
hieu110600 <tmhql2k@gmail.com>
2021-10-16 21:48:35 +0700

    noi dung tuan 3

--- Demo/Controllers/AdminController.cs
+++ Demo/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Demo.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,11 +10,52 @@ namespace Demo.Controllers
 {
     public class AdminController : Controller
     {
+        DBcontext context = new DBcontext();
         // GET: Admin
         public ActionResult AdminManage()
         {
 
             return View();
+        }
+        public ActionResult Decentralization()
+        {
+            var listEmp = context.Admins.ToList();
+            return View(listEmp);
+        }
+        public ActionResult ChangePosition(int id)
+        {
+            var emp = context.Admins.Where(p => p.idAdmin == id).SingleOrDefault();
+            return View(emp);
+        }
+        [HttpPost, ActionName("ChangePosition")]
+        public ActionResult ChangePosition(Admin e)
+        {
+            if (Session["AccountAdmin"] != null)
+            {
+                e.ngaysua = DateTime.Now;
+            }
+            context.Admins.AddOrUpdate(e);
+            context.SaveChanges();
+            return RedirectToAction("Decentralization", "Admin");
+        }
+        public ActionResult Active()
+        {
+            var listEmp = context.Admins.Where(p => p.status == 0).ToList();
+            return View(listEmp);
+        }
+        public ActionResult ActiveAdmin(int id)
+        {
+            var emp = context.Admins.FirstOrDefault(p => p.idAdmin == id);
+            emp.status = 1;
+            context.Admins.AddOrUpdate(emp);
+            context.SaveChanges();
+            return RedirectToAction("Active", "Admin");
+        }
+        [ActionName("Trash")]
+        public ActionResult Trash()
+        {
+            var listEmp = context.Admins.Where(p => p.status == 3).ToList();
+            return View(listEmp);
         }
     }
 }
